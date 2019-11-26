9c1f90b8f668e8903c878b82188a2428fd50930d
Roberto <camachof.roberto@gmail.com>
2019-11-26 20:22:13 -0300

    feat(create model): Create schema Movie Theater

--- models/Movie.js
+++ models/Movie.js
@@ -2,18 +2,23 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const movieSchema = new Schema({
-  title: { type: String, required: true },
-  original_title: String,
-  original_languange: String,
-  release_date: String,
-  genre: String,
-  overview: String,
-  poster_urls: [String],
-  trailer_urls: [String],
-  ratings: [],
-  rated: String,
-});
+const movieSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    original_title: String,
+    original_languange: String,
+    release_date: String,
+    genre: String,
+    overview: String,
+    poster_urls: [String],
+    trailer_urls: [String],
+    ratings: [],
+    rated: String,
+  },
+  {
+    timestamps: true,
+  },
+);
 
 const Movie = mongoose.model('Movie', movieSchema);
 
