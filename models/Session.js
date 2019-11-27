d88c160205a75ee34214260af76e8525bee2ffde
Roberto <camachof.roberto@gmail.com>
2019-11-26 21:39:08 -0300

    fix(model): add timestamp

--- models/Session.js
+++ models/Session.js
@@ -2,14 +2,19 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const sessionSchema = new Schema({
-  date: { type: String, required: true },
-  movie: { type: Schema.Types.ObjectId, ref: 'Movie' },
-  times: [{ type: String, required: true }],
-  room: { type: String },
-  dubbed: { type: Boolean, required: true },
-  extra_features: { type: String },
-});
+const sessionSchema = new Schema(
+  {
+    date: { type: String, required: true },
+    movie: { type: Schema.Types.ObjectId, ref: 'Movie' },
+    times: [{ type: String, required: true }],
+    room: { type: String },
+    dubbed: { type: Boolean, required: true },
+    extra_features: { type: String },
+  },
+  {
+    timestamps: true,
+  },
+);
 
 const Session = mongoose.model('Session', sessionSchema);
 
--- models/Ticket.js
+++ models/Ticket.js
@@ -2,12 +2,17 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const ticketSchema = new Schema({
-  user: { type: Schema.Types.ObjectId, ref: 'User' },
-  movie: { type: Schema.Types.ObjectId, ref: 'Movie' },
-  movie_theater: { type: Schema.Types.ObjectId, ref: 'MovieTheater' },
-  show_time: { type: String, required: true },
-});
+const ticketSchema = new Schema(
+  {
+    user: { type: Schema.Types.ObjectId, ref: 'User' },
+    movie: { type: Schema.Types.ObjectId, ref: 'Movie' },
+    movie_theater: { type: Schema.Types.ObjectId, ref: 'MovieTheater' },
+    show_time: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  },
+);
 
 const Ticket = mongoose.model('Ticket', ticketSchema);
 
