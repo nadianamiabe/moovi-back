15adf5d6e462ea9f63b540ac9102d7891bd2b3b9
nadia nami abe <abe.nami0510@gmail.com>
2019-11-28 18:40:28 -0300

    Revert "fix(model): add timestamp"
    This reverts commit d88c160205a75ee34214260af76e8525bee2ffde.

--- models/MovieTheater.js
+++ models/MovieTheater.js
@@ -2,19 +2,14 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const movieTheaterSchema = new Schema(
-  {
-    name: { type: String, required: true },
-    address: { type: String, required: true },
-    location: { lat: Number, lng: Number },
-    sessions: [{ type: Schema.Types.ObjectId, ref: 'Session' }],
-    place_id: { type: String, required: true },
-    phone_number: { type: String }
-  },
-  {
-    timestamps: true
-  }
-);
+const movieTheaterSchema = new Schema({
+  name: { type: String, required: true },
+  address: { type: String, required: true },
+  location: { lat: Number, lng: Number },
+  sessions: [{ type: Schema.Types.ObjectId, ref: 'Session' }],
+  place_id: { type: String, required: true },
+  phone_number: { type: String },
+});
 
 const MovieTheater = mongoose.model('MovieTheater', movieTheaterSchema);
 
