244782aae337d0e251c545e45e4bb483d7956ce3
Nadia Abe <abe.nami0510@gmail.com>
2019-12-04 22:33:40 -0300

    fix(token): api.routes

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -1,48 +1,54 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const userRoutes = require('./user.routes');
-const index = require('./index.routes');
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const userRoutes = require("./user.routes");
+const index = require("./index.routes");
 const movieTheatersRoutes = require("./movieTheaters.routes");
 const movies = require("./movies.routes");
-
-
 
 const router = express.Router();
 
 const verifyLoggedAreaToken = () => (req, res, next) => {
-  const authHeader = req.header('Authorization');
+  const authHeader = req.header("Authorization");
 
-  const token = authHeader.split(' ')[1];
+  if (!authHeader) {
+    res.status(401).json({ message: "Token não enviado" });
+    return;
+  }
+  const token = authHeader.split(" ")[1];
 
   try {
     const tokenInfo = jwt.verify(token, process.env.token);
-    const { username, id, isSubscribed, customerId, subscriptionId } = tokenInfo;
+    const {
+      username,
+      id,
+      isSubscribed,
+      customerId,
+      subscriptionId
+    } = tokenInfo;
     req.user = {
       username,
       id,
       isSubscribed,
       customerId,
-      subscriptionId,
+      subscriptionId
     };
 
     next();
   } catch (error) {
-    res.status(401).json({ message: 'Token expirado', status: 401 });
+    res.status(401).json({ message: "Token expirado", status: 401 });
     console.log(error);
   }
 };
 
-router.use('/users', userRoutes);
+router.use("/", index);
+router.use("/users", userRoutes);
 
-router.use('/movie-theater', movieTheatersRoutes);
+router.use("/movie-theater", movieTheatersRoutes);
 router.use(verifyLoggedAreaToken());
 
+const payments = require("../routes/payments.routes");
 
-const payments = require('../routes/payments.routes');
-
-router.use('/payments', payments);
-router.use('/', index);
+router.use("/payments", payments);
 router.use("/movies", movies);
-
 
 module.exports = router;
