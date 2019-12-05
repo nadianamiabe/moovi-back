4ab8b14c2740619e69775c6155a1d20dfa938275
Roberto <camachof.roberto@gmail.com>
2019-12-05 20:15:42 -0300

    fix(login): fix alert login

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -1,29 +1,20 @@
-<<<<<<< HEAD
-const express = require("express");
-const jwt = require("jsonwebtoken");
-const userRoutes = require("./user.routes");
-const index = require("./index.routes");
-const movieTheatersRoutes = require("./movieTheaters.routes");
-const movies = require("./movies.routes");
-=======
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const userRoutes = require('./user.routes');
 const index = require('./index.routes');
 const movieTheatersRoutes = require('./movieTheaters.routes');
 const movies = require('./movies.routes');
->>>>>>> 936fd64b291ebaf2c026e57849ec53f5fbb33387
 
 const router = express.Router();
 
 const verifyLoggedAreaToken = () => (req, res, next) => {
-  const authHeader = req.header("Authorization");
+  const authHeader = req.header('Authorization');
 
   if (!authHeader) {
-    res.status(401).json({ message: "Token não enviado" });
+    res.status(401).json({ message: 'Token não enviado' });
     return;
   }
-  const token = authHeader.split(" ")[1];
+  const token = authHeader.split(' ')[1];
 
   try {
     const tokenInfo = jwt.verify(token, process.env.token);
@@ -44,28 +35,21 @@ const verifyLoggedAreaToken = () => (req, res, next) => {
 
     next();
   } catch (error) {
-    res.status(401).json({ message: "Token expirado", status: 401 });
+    res.status(401).json({ message: 'Token expirado', status: 401 });
     console.log(error);
   }
 };
 
-router.use("/", index);
-router.use("/users", userRoutes);
+router.use('/', index);
+router.use('/users', userRoutes);
 
-router.use("/movie-theater", movieTheatersRoutes);
+router.use('/movie-theater', movieTheatersRoutes);
 router.use(verifyLoggedAreaToken());
 
-const payments = require("../routes/payments.routes");
-
-<<<<<<< HEAD
-router.use("/payments", payments);
-router.use("/movies", movies);
-=======
 const payments = require('../routes/payments.routes');
 
 router.use('/payments', payments);
 router.use('/', index);
 router.use('/movies', movies);
->>>>>>> 936fd64b291ebaf2c026e57849ec53f5fbb33387
 
 module.exports = router;
