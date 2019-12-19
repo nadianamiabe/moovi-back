7e70d14d862b0cfa7f4480fb634b7edd32264088
Pedro Chiossi <pchiossi@gmail.com>
2019-12-19 12:03:31 -0300

    fix find movie theaters that have joined names

--- controllers/session.controller.js
+++ controllers/session.controller.js
@@ -5,21 +5,27 @@ const clean = (word) => {
   return word.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
 };
 
+const splitCamelCase = (word) => {
+  return word.match(/(^[a-z]|[A-Z0-9])[a-z]*/g);
+};
 
 const getMovieSessions = async (req, res) => {
   const { id, city } = req.params;
-  console.log(city);
 
   try {
     const cinema = await MovieTeather.findById(id);
-    const words = cinema.name.replace(/Cinemark\s|Shopping\s/g, '').split(' ');
-    console.log(words);
+    let words = cinema.name.replace(/Cinemark\s|Shopping\s/g, '').split(' ');
+    if (words.length === 1) {
+      const split = splitCamelCase(words[0]);
+      if (split) {
+        words = split;
+      }
+    }
     const regex = words.reduce((acc, value) => {
       return acc + '|' + value;
     });
-
+    console.log(regex);
     const sessions = await Session.find({ city: city.toLowerCase(), cinema: { $regex: clean(regex), $options: 'i' } });
-    console.log(sessions);
     if (sessions) {
       res.status(200).json(sessions);
     }
